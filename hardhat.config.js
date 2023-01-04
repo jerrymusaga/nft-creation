dd407ba563058f42f6e2d8cd59175df84609e03d
jerrymusaga <musagatechnology@gmail.com>
2023-01-04 01:49:52 +0100

    contract init

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    url: process.env.GOERLI_URL,
+    accounts: [process.env.PRIVATE_KEY]
+  }
 };
